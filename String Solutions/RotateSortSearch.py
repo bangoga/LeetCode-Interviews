0e071e97867e503d23812908b9ed4d209366d046
Khalil Mohsin <mohsin.khalil8@gmail.com>
2020-04-14 23:48:11 -0400

    Partial solution using binary search -> only searching less than values

--- String Solutions/RotateSortSearch.py
+++ String Solutions/RotateSortSearch.py
@@ -4,30 +4,33 @@ Created on Tue Apr 14 19:10:41 2020
 
 @author: mohsi
 """
+def sol(nums,target):
+    start=0
+    end=len(nums)-1
+    ans = recursive(nums,start,end,target)
+    print ("index is {0}".format(ans))
+    
 
-def recursive(nums,x,target):
+## Change this to involve more and less values ad well aka add comparators 
+def recursive(nums,p1,p2,target):
     
-    p1= len(nums)//2 -1
-    p2 = p1+1
+    if(p2-p1 < 2):
+        if(nums[p2]==target): return p2 
+        elif(nums[p1]==target): return p1 
+        else: return -1
     
-    if(len(nums)<=3):
-        if(nums[0]==target or nums[1] == target):
-            return 0
-            
-        else: 
-            return -1
-        
+    mid=((p2-p1)//2 + p1)
+    print(mid)
+    if(nums[mid-1] == target): return mid-1
+    elif(nums[mid] == target): return mid
+    elif(nums[mid+1] == target): return mid+1
     
-    if(nums[p1]<nums[p2]):
-        print(nums[p1],p1,nums[p2],p2)
-        
+    return recursive(nums,mid+1,p2,target)
     
-    return recursive(nums[p1:len(nums)],p1,target)
     
 
 
 arr=[4,5,6,7,0,1,2]
-p1= len(arr)//2 -1
-p2 = p1+1
-arr[p1:len(arr)]
-recursive(arr,0,0)
+arr=[7,8,9,10,11,12,1,3]
+
+sol(arr,11)
