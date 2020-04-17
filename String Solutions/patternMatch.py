31e94238a06a6372996dfee1dd9b29320fc940c0
Khalil Mohsin <mohsin.khalil8@gmail.com>
2020-04-17 00:01:57 -0400

    chanegs

--- String Solutions/patternMatch.py
+++ String Solutions/patternMatch.py
@@ -5,7 +5,7 @@ Created on Thu Apr 16 23:49:58 2020
 @author: mohsi
 """
 
-def isMatch(self, s: str, p: str) -> bool:
+def isMatch(s,p):
     """
     Take queues pattern_queue and string_queue
     
@@ -20,4 +20,29 @@ def isMatch(self, s: str, p: str) -> bool:
         - repeat, if at any point *, continue till next pattern string_char
     
     """
+    # -> Covert to lower case 
+    s=s.lower()
+    p=p.lower()
+    
+    # -> Convert to a queue 
+    pattern_queue=[char for char in p]
+    string_queue=[char for char in s]
+    
+    i=0;
+    j=0;
+    
+    alphabets = "abcdefghijklmnopqrstuvwxyz"
+    preceding = ""
+    while(i<len(pattern_queue)):
+        pattern_char = p[i]
+        string_char  = s[j]
         
+        if(pattern_char in alphabets):
+            print(p)
+        
+        if
+    
+        preceding    = string_char
+    
+    
+isMatch("ss","s*")
