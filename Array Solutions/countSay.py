1aa40728418af83477036dfa678461d94027efc0
Khalil Mohsin <mohsin.khalil8@gmail.com>
2020-05-07 20:04:05 -0400

    count and say function

---
+++ Array Solutions/countSay.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  7 19:35:54 2020
+
+@author: mohsi
+"""
+
+# =============================================================================
+#   The number n is between 1 and 30
+#   count in the lowest possible way a number can be read
+#   1 is read off as "one 1" or 11.
+#   11 is read off as "two 1s" or 21.
+#   21 is read off as "one 2, then one 1" or 1211.
+
+#   think recursively
+#   Base case = 1 
+
+# =============================================================================
+def countAndSay(n):
+    
+    if (n == 1):
+        return "1"
+
+    ret_string = ""
+    
+    return ret_string
+
+
+
+# =============================================================================
+# 
+# 
+# 
+# =============================================================================
